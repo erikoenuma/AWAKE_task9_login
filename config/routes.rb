495d7a96fc22b55bb28c0dedbc9ccfcf17af737a
erikoenuma <koenuma.eri@gmail.com>
2022-04-15 18:52:50 +0900

    routes.db修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'tasks#index'
+  root 'tasks#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
   resources :users, only: [:new, :create, :show, :destroy, :edit, :update]
