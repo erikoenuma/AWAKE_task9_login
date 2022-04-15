0ecf06bc1aebb962bfaaae2d88fb30b3e0f02f3e
erikoenuma <koenuma.eri@gmail.com>
2022-04-15 15:48:28 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 30 }
     validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, presence: true
     before_validation { email.downcase! }
     has_secure_password
     has_many :tasks, dependent: :destroy
